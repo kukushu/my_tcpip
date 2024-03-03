d8e05d4d1d9451db4e8e435c52bf9c0c690a4780
kukushu <1693479180@qq.com>
2024-03-03 13:52:48 +0800

    producer-consumer

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -2,31 +2,44 @@
 #include "sys_plat.h"
 #include <stdatomic.h>
 
-static sys_sem_t sem;
+static sys_sem_t read_sem, write_sem;
 static sys_mutex_t mutex;
 static int count;
 
+
+
+static char test_buffer[100];
+static int write_index, read_index;
+
 void thread1(void * string) {
-    for (int i = 0; i < 1000000; i ++) {
-        sys_mutex_lock(mutex);
-        count ++;
-        sys_mutex_unlock(mutex);
+    for (int i = 0; i < 2 * sizeof(test_buffer); i ++) {
+        sys_sem_wait(read_sem, 0);
+        uint8_t data = test_buffer[read_index ++];
+        if (read_index >= sizeof(test_buffer)) {
+            read_index = 0;
+        }
+        plat_printf("thread 1 : read data %d\n", data);
+        sys_sem_notify(write_sem);
+        sys_sleep(200);
     }
-    plat_printf("thread1 %d\n", count);
 }
 void thread2(void * string) {
-    for (int i = 0; i < 1000000; i ++) {
-        sys_mutex_lock(mutex);
-        count --;
-        sys_mutex_unlock(mutex);
+    for (int i = 0; i < 2 * sizeof(test_buffer); i ++) {
+        sys_sem_wait(write_sem, 0);
+        test_buffer[write_index ++] = i;
+        if (write_index >= sizeof(test_buffer)) {
+            write_index = 0;
+        }
+        plat_printf("thread 2 : write data = %d\n", i);
+        sys_sem_notify(read_sem);
     }
-    plat_printf("thread2 %d\n", count);
 }
 
 int main (void) 
 {
     mutex = sys_mutex_create();
-    count = 0;
+    read_sem = sys_sem_create(0);
+    write_sem = sys_sem_create(100);
     sys_thread_create(thread1, "AAAAA");
     sys_thread_create(thread2, "BBBBB");
 
