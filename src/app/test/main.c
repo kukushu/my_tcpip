ea1fe1e28cdbe7c71a90a631ca28c4d91ded7717
kukushu <1693479180@qq.com>
2024-03-04 12:54:29 +0800

    debug system

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -1,15 +1,26 @@
 #include "sys_plat.h"
 #include "net.h"
 #include "netif_pcap.h"
-
+#include "dbg.h"
+#include "net_cfg.h"
 
 net_err_t netdev_init (void) {
     netif_pcap_open();
     return NET_ERR_OK;
 }
 
+void dbg_test (void) {
+    dbg_error(DBG_TEST, "dbg_info : %s\n", "just_for_test");
+    dbg_warning(DBG_TEST, "dbg_info : %s\n", "just_for_test");
+    dbg_info(DBG_TEST, "dbg_info : %s\n", "just_for_test");
+    dbg_debug(2 == 1, "failed");
+
+}
+
+
 int main (void) 
 {
+    //dbg_test();
     net_init();
     net_start();
 
---
+++ src/net/net/net_cfg.h
@@ -0,0 +1,13 @@
+#ifndef NET_CFG_H
+#define NET_CFG_H
+
+#define DBG_TEST        DBG_LEVEL_INFO
+
+
+
+
+
+
+
+
+#endif
